5ccbab9288852f959bb24359defac6528f2c32cb
marcos oliveira <marcossantosoliveira@yahoo.com.br>
2025-04-03 14:27:18 -0300

    exercicio2

--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+
 
 public class Program
 {
@@ -23,8 +25,71 @@ public class Program
 
     public static int getTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
+        int numberPages = GoalBalanceClientAsync(team, year, 1).Result.total_pages;
+
+        int goals = 0;
+
+        for (int i = 1; i <= numberPages; i++)
+        {
+            var response = GoalBalanceClientAsync(team, year, i).Result;
+
+            goals += (from p in response.data
+                          select int.Parse(p.team1goals)).Sum();
+                   
+        }       
+
+        return goals;
     }
 
+    public static async Task<ApiResponse> GoalBalanceClientAsync(string team, int year, int page)
+    {
+        string apiUrl = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team1={team}&page={page}";
+        var result = new ApiResponse();
+
+        try
+        {
+            
+            using (HttpClient client = new HttpClient())
+            {
+               
+                HttpResponseMessage response = await client.GetAsync(apiUrl);             
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                                     
+                    result = JsonSerializer.Deserialize<ApiResponse>(responseBody);
+
+                    
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
+
+        return result;
+    }
+
+}
+
+public class FootballMatch
+{
+    public string competition { get; set; }
+    public int year { get; set; }
+    public string round { get; set; }
+    public string team1 { get; set; }
+    public string team2 { get; set; }
+    public string team1goals { get; set; }
+    public string team2goals { get; set; }
+}
+
+public class ApiResponse
+{
+    public int page { get; set; }
+    public int per_page { get; set; }
+    public int total { get; set; }
+    public int total_pages { get; set; }
+    public List<FootballMatch> data { get; set; }
 }
